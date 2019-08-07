1d50662bfee98b152f7996e9dced89a2c6c956f3
SimonZhouIptv <SimonZhouIptv>
2019-08-07 16:59:00 +0800

    first commit

---
+++ dnw_driver_win7-64/inf64/install.bat
@@ -0,0 +1,3 @@
+devcon remove "USB\VID_04E8&PID_1234"
+devcon install secbulk.inf "USB\VID_04E8&PID_1234"
+pause
---
+++ dnw_driver_win7-64/inf64/remove.bat
@@ -0,0 +1,4 @@
+devcon remove "USB\VID_04E8&PID_1234"
+pause
+erase C:\windows\system32\drivers\secbulk.sys
+pause
